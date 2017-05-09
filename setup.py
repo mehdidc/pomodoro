73287bd4163643ff706313f0dcb575b4fa1e28d9
Mehdi Cherti <mehdicherti@gmail.com>
2017-05-09 16:01:12 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="pomodoro",
+    version="0.1",
+    author="mehdi cherti",
+    author_email="mehdicherti@gmail.com",
+    description=(""),
+    license="MIT",
+    keywords="",
+    url="",
+    zip_safe=False,  # the package can run out of an .egg file
+    classifiers=['Intended Audience :: Science/Research',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development',
+                 'Topic :: Scientific/Engineering',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS'],
+    platforms='any',
+    scripts=['pomodoro', 'pomostat']
+)
