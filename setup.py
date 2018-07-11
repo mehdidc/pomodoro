04878dc3a1031527621affaac5a83efabfb0b727
mehdi cherti <mehdicherti@gmail.com>
2018-07-11 19:10:00 +0200

    new version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ The code is based on : http://code.activestate.com/recipes/577358-pomodoro-timer
 
 setup(
     name="pomodoro-cli",
-    version="0.1.7",
+    version="0.1.8",
     author="mehdi cherti",
     author_email="mehdicherti@gmail.com",
     description=description,
