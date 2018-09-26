4577988592564282ec8672f1a7cea934677adf12
mehdi cherti <mehdicherti@gmail.com>
2018-09-26 10:51:36 +0200

    fix bug in pomostat today and yesterday

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ The code is based on : http://code.activestate.com/recipes/577358-pomodoro-timer
 
 setup(
     name="pomodoro-cli",
-    version="0.1.8",
+    version="0.1.9",
     author="mehdi cherti",
     author_email="mehdicherti@gmail.com",
     description=description,
